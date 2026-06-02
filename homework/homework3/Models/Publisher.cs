993b57d83160029d44689a8c2d67ac8b2a440870
Alexandra125-creat <kaa25u369@student.bmstu.ru>
2026-06-02 11:45:28 +0300

    Add files via upload

---
+++ homework/homework3/Models/Publisher.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Homework3.Models
+{
+    public class Publisher
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<Journal> Journals { get; set; } = new List<Journal>();
+
+        public Publisher() { }
+
+        public Publisher(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString() => $"{Name}";
+    }
+}
