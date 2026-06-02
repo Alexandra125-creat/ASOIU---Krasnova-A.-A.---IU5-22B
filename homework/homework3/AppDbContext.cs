7dfa92b0f3bb6c505b2aed8795d1e0b1bd76b2a5
Alexandra125-creat <kaa25u369@student.bmstu.ru>
2026-06-02 11:44:19 +0300

    Add files via upload

---
+++ homework/homework3/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Homework3.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Journal> Journals { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbPath = Path.Combine(AppContext.BaseDirectory, "app.db");
+            optionsBuilder.UseSqlite($"Data Source={dbPath}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Journal>()
+                .HasOne(j => j.Publisher)
+                .WithMany(p => p.Journals)
+                .HasForeignKey(j => j.PublisherId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Publisher>()
+                .HasIndex(p => p.Name)
+                .IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
